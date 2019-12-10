095387ce77c9ad0880d40bced9626ee9b84b69e8
= <amitkrishna936@gmail.com>
2019-12-10 19:44:47 +0530

    hhsksls

---
+++ EPMS.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+struct Inumber{
+    char I;
+    int no;
+}inumber;
+struct Employee{
+    static int at_counter;
+    int Age;
+    char address[10];
+    int  contact_number; 
+}emp;
+int main()
+{
+    printf("Enter your option \n 1:Swipe-In \n 2:Search Employee \n 3:Enquire Employee \n 4:Statistics \n 5:Swipe Out"); 
+    //2.	Who all came within a range of IDs, and how often they entered?
+    //3 	Did a particular employee come today
+    //3.	Name of the employees who are all out for a particular time period
+    //4. 	How many employees came today? 
+    //4.	Which employee moves out of office most number of times?
+    //2.	How often did an employee enter into the office?
+
+}
+int swipe-in()
+{
+ return    
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Mini-Project
+# EmployeePresenceManagementSystem
+EPMS C programming Assignment
